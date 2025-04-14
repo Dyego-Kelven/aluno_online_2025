cc0c532f460d36ab0d51c69b8418092221ae4630
Dyego Kelven <dkelvens@hotmail.com>
2025-04-14 20:32:47 -0300

    Testes do aluno

--- src/main/java/br/com/alunoonline/api/service/AlunoService.java
+++ src/main/java/br/com/alunoonline/api/service/AlunoService.java
@@ -3,8 +3,12 @@ package br.com.alunoonline.api.service;
 import br.com.alunoonline.api.model.Aluno;
 import br.com.alunoonline.api.repository.AlunoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class AlunoService {
@@ -12,7 +16,37 @@ public class AlunoService {
     @Autowired
     AlunoRepository alunoRepository;
 
-    public void criarAluno(Aluno aluno) {
+    public void createAluno(Aluno aluno){
+
         alunoRepository.save(aluno);
     }
+
+    public List<Aluno> listAllAlunos(){
+        return alunoRepository.findAll();
+    }
+
+    public Optional<Aluno> searchAlunoById(Long idAluno){
+        return alunoRepository.findById(idAluno);
+    }
+
+    public void deleteAlunoById(Long idAluno){
+        alunoRepository.deleteById(idAluno);
+    }
+
+    public void updateAlunoById(Long idAluno, Aluno aluno){
+        Optional<Aluno> alunoDataBase = searchAlunoById(idAluno);
+
+        if (alunoDataBase.isEmpty()){
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Aluno não encontrado do banco de dados");
+        }
+
+        Aluno alunoEdit = alunoDataBase.get();
+
+        alunoEdit.setNomeAluno(aluno.getNomeAluno());
+        alunoEdit.setCpfAluno(aluno.getCpfAluno());
+        alunoEdit.setEmailAluno(aluno.getEmailAluno());
+
+        alunoRepository.save(alunoEdit);
+    }
+
 }
